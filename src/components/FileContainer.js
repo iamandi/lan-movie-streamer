350519378a0f8c16f6efc2bc98a16f99aec10b22
iamandi <aakawade9@gmail.com>
2020-11-17 02:19:52 -0800

    fix case sensitive sorting issue

--- src/components/FileContainer.js
+++ src/components/FileContainer.js
@@ -28,9 +28,10 @@ function FileContainer() {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(4);
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState(null);
   const [sortColumn, setSortColumn] = useState({ path: "name", order: "asc" });
 
-  const [totalCount, setTotalCount] = useState(0);
+  //const [totalCount, setTotalCount] = useState(0);
   const [error, setError] = useState("");
   const [arrangement, setArrangement] = useState("list");
 
@@ -51,8 +52,8 @@ function FileContainer() {
     */
   }, []);
 
-  const handleSearch = () => {
-    console.log("handleSearch");
+  const handleArrangement = (e) => {
+    setArrangement(e.toString());
   };
 
   const handleLike = (file) => {
@@ -63,26 +64,66 @@ function FileContainer() {
     setDirTree(files);
   };
 
-  const handleDelete = () => {
-    console.log("handleDelete");
+  const handleSearch = () => {
+    console.log("handleSearch");
   };
 
-  const handleSort = () => {
-    console.log("handleSort");
+  const handleSort = (sortColumn) => {
+    setSortColumn(sortColumn);
   };
 
-  const handleArrangement = (e) => {
-    console.log("handleArrangement", e);
-    setArrangement(e.toString());
-  };
+  const getPagedData = () => {
+    /*
+    const {
+      pageSize,
+      currentPage,
+      sortColumn,
+      selectedGenre,
+      searchQuery,
+      movies: allMovies,
+    } = this.state;
+    */
+    //const [dirTree, setDirTree] = useState([]);
+    //const [currentPage, setCurrentPage] = useState(1);
+    //const [pageSize, setPageSize] = useState(4);
+    //const [searchQuery, setSearchQuery] = useState("");
+    //const [selectedGenre, setSelectedGenre] = useState(null);
+    //const [sortColumn, setSortColumn]
 
-  const selectedGenre = "Action";
+    let filtered = dirTree;
+    if (searchQuery)
+      filtered = dirTree.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = dirTree.filter((m) => m.genre._id === selectedGenre._id);
+
+    /// Differentiate between name and other columns
+    let sorted;
+    if (sortColumn.path === "name")
+      sorted = _.orderBy(
+        filtered,
+        [(sortColumn) => sortColumn.path.toLowerCase()],
+        [sortColumn.order]
+      );
+    else sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const files = sorted; //paginate(sorted, currentPage, pageSize);
+
+    const directoryCount = files.filter((o) => o.type === "directory").length;
+    const fileCount = files.filter((o) => o.type === "file").length;
+
+    return {
+      totalCount: filtered.length,
+      directoryCount,
+      fileCount,
+      data: files,
+    };
+  };
 
   console.log("dirTree", dirTree);
-  console.log(">>>>>>>> dirTree.length", dirTree.length);
 
-  const directories = dirTree.filter((o) => o.type === "directory");
-  const files = dirTree.filter((o) => o.type === "file");
+  const { directoryCount, fileCount, data: files } = getPagedData();
 
   return (
     <div className='row'>
@@ -112,11 +153,11 @@ function FileContainer() {
           </div>
 
           <div className='h2-title'>
-            <div class='form-group' style={{ width: "100%" }}>
+            <div className='form-group' style={{ width: "100%" }}>
               <h5>Overview</h5>
               <hr />
               <div style={{ display: "flex", justifyContent: "space-between" }}>
-                {`Showing ${directories.length} directories and ${files.length} files.`}
+                {`Showing ${directoryCount} directories and ${fileCount} files.`}
                 <Option
                   onSelect={handleArrangement}
                   arrangement={arrangement}
@@ -125,14 +166,14 @@ function FileContainer() {
             </div>
           </div>
 
-          {_.isEmpty(dirTree) ? (
+          {_.isEmpty(files) ? (
             <div className='table-container'>
               <Loader type='Puff' color='#00BFFF' height={100} width={100} />
             </div>
           ) : (
             <FilesArrangement
               arrangement={arrangement}
-              dirTree={dirTree}
+              dirTree={files}
               sortColumn={sortColumn}
               handleSort={handleSort}
               handleLike={handleLike}
